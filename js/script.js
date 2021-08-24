3fad43cae37117954d485a9cefa97ec8bb00d061
nergiz2301 <76102036+nergiz2301@users.noreply.github.com>
2021-08-24 22:14:30 +0300

    Properties released

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const overview=document.querySelector(".overview");
 //overview profile info appears
 const username= "nergiz2301";
-const repoList= document.querySelector(".repo-list")
+const repoList= document.querySelector(".repo-list");
+const reposs=document.querySelector(".repos");
+const repoData=document.querySelector(".repo-data")
 const gitUserInfo= async function () {
     const userInfo= await fetch(`https://api.github.com/users/${username}`);
     const data= await userInfo.json();
@@ -41,5 +43,44 @@ const displayRepos= function(repos){
     repoItem.innerHTML=`<h3>${repo.name}</h3>`;
     repoList.append(repoItem);
   }
+};
+
+repoList.addEventListener("click", function(e){
+  if (e.target.matches("h3")){
+  const repoName=e.target.innerText;
+  gitRepoName(repoName);
+  }
+});
+
+const gitRepoName= async function(repoName){
+  const firstRepo= await fetch (`https://api.github.com/repos/${username}/${repoName}`);
+  const repoInfo= await firstRepo.json();
+  console.log(repoInfo);
+
+const fetchLanguages= await fetch(`https://github.com/nergiz2301/javascript-potluck`);
+const languageData= await fetchLanguages.json();
+
+const Languages= [];
+for (const language in languageData){
+  Langauges.push(language);
 }
+ displayRepoInfo(repoInfo,languages)
+};
+
+const displayRepoInfo= function(repoInfo,languages){
+  repoData.innerHTML="";
+  repoData.classList.remove("hide");
+  reposs.classList.add("hide");
+  const div=document.createElement("div");
+  div.innerHTML=
+  `<h3>Name: ${repoInfo.name}</h3>
+  <p>Description: ${repoInfo.description}</p>
+  <p>Default Branch: ${repoInfo.default_branch}</p>
+  <p>Languages: ${languages.join(", ")}</p>
+  <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+`;
+repoData.append(div);
+};
+
+
 
